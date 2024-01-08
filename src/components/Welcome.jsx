884d6b1f82159f550d1d19d94bcc481ab956d955
Bruno Leal <brunolealmaciel@gmail.com>
2024-01-08 02:35:32 -0300

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -13,7 +13,6 @@ const Welcome = () => {
         <h2>Welcome</h2>
         <p>Clique no botao abaixo para começar</p>
         <button onClick={()=> dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
-        <img src="https://raw.githubusercontent.com/matheusbattisti/quiz_react/3234b0fd070c26ef972b70116362ab8950928b71/src/img/quiz.svg" alt="Inicio do quiz" />
     </div>
   )
 }
